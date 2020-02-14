f80d1a29b5108c0246ea79f698804a08551570f7
Brenda-M <brendanjuguna1@gmail.com>
2020-02-14 17:18:27 +0300

    style jumbotron

---
+++ script.js
@@ -0,0 +1,13 @@
+function akanName(){
+  var Year= document.getElementById("n").value;
+  var CC= parseInt("id for year".slice(0,2));
+  var YY= parseInt ("id for year" slice(0,2));
+  var DD= document.getElementById("id for date").value;
+  var MM= document.getElementById("id for month").value;
+}
+
+var maleNames= ["Kwasi", "Kwadwo","Kwabena", "Kwaku", "Yaw","Kofi", "Kwame"];
+var femaleNames= ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ];
+var birthDays= ["sunday","monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+
+if () 
