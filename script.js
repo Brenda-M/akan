8feed7914b9a46eca191ebe8fb9e66eacb923a7b
Brenda-M <brendanjuguna1@gmail.com>
2020-02-16 17:27:37 +0300

    recreate index file and javascript file

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-function birthdate(){
-  var Year= document.getElementById("n").value;
-  var CC= parseInt(year.slice(0,2));
-  var YY= parseInt (year.slice(2.));
-  var DD= document.getElementById("date").value;
-  var MM= document.getElementById("month").value;
-  var myGender=document.getElementById("gender").value;
+function birthDate(){
+  var year= document.getElementById("year").value;
+  var cc = parseInt(year.slice(0,2));
+  var yy= parseInt (year.slice(2.));
+  var dd= document.getElementById("date").value;
+  var mm= document.getElementById("month").value;
 
 }
 
-var dayOfTheWeek= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
-
-
+var dayOfTheWeek= (((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd )% 7;
 var maleNames= ["Kwasi", "Kwadwo","Kwabena", "Kwaku", "Yaw","Kofi", "Kwame"];
 var femaleNames= ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ];
 var birthDays= ["sunday","monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-
+var myGender =document.getElementByName("gender").value;
 
 
 if(myGender==="Male")
 {
   document.getElementById("display").innerHTML= "Your akan names is"+ maleNames+" because you were born on a"+ daysOfTheWeek;
   }
-else if(gender==="Female"){
+else if (gender==="Female"){
     document.getElementById("display").innerHTML= "Your akan names is"+ femaleNames+" because you were born on a"+ daysOfTheWeek;
   }
 
-
-// var myGender= document.getElelementByName ("gender");
+  else{
+          alert("Invalid Entry");
+          document.getElementById("display").innerHTML="";
+        }
