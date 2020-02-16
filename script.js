1b01a83da46ba909d9bbf5f3bba7ed40e64b9164
Brenda-M <brendanjuguna1@gmail.com>
2020-02-16 22:05:06 +0300

    switch to using java script methods for day and time

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 function birthDate(){
-  const YEAR= document.getElementById("year").value;
-  const CC = (YEAR-1)/100+1; // parseInt(YEAR.slice(0,2));
+  //obtain values entered in the forms
+
   const DD= document.getElementById("date").value;
   const MM= document.getElementById("month").value;
-
+  const YY= document.getElementById("year").value;
   const myGender = document.getElementById("gender").value;
 
+  // Check if the values entered are numbers
+  if (!parseInt(DD)) {
+    return alert("The date you entered is not valid. Make sure it is a number and try again.");
+  }
 
-  const dayOfTheWeek =  Math.round(( CC/4 -2*CC-1 + 5*YEAR/4  + 26*(MM+1)/10 + DD ) % 7);
+  if (!parseInt(MM)) {
+    return alert("The month you entered is not valid. Make sure it is a number and try again.");
+  }
+
+  if (!parseInt(YY)) {
+    return alert("The year you entered is not valid. Make sure it is a number and try again.");
+  }
+
+  // Check if the values are valid
+  if (DD < 1 || DD > 31) {
+    return alert("The date you entered is not valid.");
+  }
+
+  if (MM < 1 || MM > 12) {
+    return alert("The month you entered is not valid.");
+  }
+
+  // Get the current year
+  const currentYear = new Date().getFullYear();
+
+  if (YY < 1800 || YY > currentYear) {
+    return alert("The year you entered is not valid.");
+  }
+
+  // Get the century digits
+  const CC = YY.toString().substring(0, 2);
+
+  //Get the day of the week
+  const dayOfTheWeek = userDate.getDay();
 
   const maleNames = ["Kwasi", "Kwadwo","Kwabena", "Kwaku", "Yaw","Kofi", "Kwame"];
   const femaleNames= ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ];
@@ -24,6 +56,6 @@ function birthDate(){
   } else {
     name = femaleNames[dayOfTheWeek];
   }
-  
+
   document.getElementById("display").innerHTML= `Your akan names is ${name} because you were born on a ${day}`;
 }
