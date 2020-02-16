91f4a0f31f5910134855071dc3ca08ef3e09b7f9
Brenda-M <brendanjuguna1@gmail.com>
2020-02-16 21:00:11 +0300

    create README document for project

---
+++ README.md
@@ -0,0 +1,56 @@
+##Project Name
+Akan Name Generator
+
+##Description
+The Akan Name Generator is a web application that enable users to identify their Akan names by simply inputting their date of birth and gender. Akan names are names often used by the Akan people of Ghana to name their children after the day of the week they were born.
+
+## Technologies Used
+* HTML
+* CSS
+* bootsrap
+* Javascript
+
+
+##Author(s) information.
+
+[**Muthoni Njuguna**](https://github.com/Brenda-M)
+
+
+##Setup instructions
+- Clone this repository to your desired location in your computer. `git clone https://github.com/Brenda-M/akan.git`
+- Open terminal command line then navigate to the root folder of the application. `cd Akan_Name_Generator`
+- Open `index.html` on your Browser.
+
+
+## Development
+
+If you would like to contribute to this project, follow the following steps: 
+- Fork the repo
+- Create a new branch
+- Make the appropriate changes in the files
+- Add changes to reflect the changes made
+- Commit your changes
+- Push to the branch
+- Create a Pull Request
+
+
+##BDD
+1. Displays Form For Entering Date and Gender
+   - INPUT: "User's Birthdate Entered"
+   - INPUT: "User's Gender Entered"
+   - OUTPUT: "Displays User's Akan Name According to Submitted Date and Gender"
+2. Displays an Error Message if Date Input Left Blank
+   - INPUT: ""
+   - OUTPUT: "Displays Error Message"
+
+##Contact Information
+- if you have any feedback, you can reach me through:
+  - brendanjuguna1@gmail.com@gmail.com
+  - 0704281246
+
+## Licence
+
+The MIT License (MIT)
+
+Copyright (c) 2020 Muthoni Njuguna.
+
--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 function birthDate(){
   const YEAR= document.getElementById("year").value;
-  const CC = parseInt(YEAR.slice(0,2));
-  const YY= parseInt (YEAR.slice(2.));
+  const CC = (YEAR-1)/100+1; // parseInt(YEAR.slice(0,2));
   const DD= document.getElementById("date").value;
   const MM= document.getElementById("month").value;
+
   const myGender = document.getElementById("gender").value;
 
-var dayOfTheWeek= (((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd )% 7;
-var maleNames= ["Kwasi", "Kwadwo","Kwabena", "Kwaku", "Yaw","Kofi", "Kwame"];
-var femaleNames= ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ];
-var birthDays= ["sunday","monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+
+  const dayOfTheWeek =  Math.round(( CC/4 -2*CC-1 + 5*YEAR/4  + 26*(MM+1)/10 + DD ) % 7);
+
+  const maleNames = ["Kwasi", "Kwadwo","Kwabena", "Kwaku", "Yaw","Kofi", "Kwame"];
+  const femaleNames= ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ];
+  const birthDays = ["sunday","monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 
 
+  const day = birthDays[dayOfTheWeek]
 
-if(myGender==="Male")
-{
-  document.getElementById("display").innerHTML= "Your akan names is"+ maleNames+" because you were born on a"+ daysOfTheWeek;
+
+  let name = '';
+
+  if(myGender === "male"){
+    name = maleNames[dayOfTheWeek];
+  } else {
+    name = femaleNames[dayOfTheWeek];
   }
-else if (gender==="Female"){
-    document.getElementById("display").innerHTML= "Your akan names is"+ femaleNames+" because you were born on a"+ daysOfTheWeek;
-  }
-
-  else{
-          alert("Invalid Entry");
-          document.getElementById("display").innerHTML="";
-        }
+  
+  document.getElementById("display").innerHTML= `Your akan names is ${name} because you were born on a ${day}`;
+}
