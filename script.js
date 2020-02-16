a307684b2388e99cc21b0097b2f44544e74037d2
Brenda-M <brendanjuguna1@gmail.com>
2020-02-16 17:34:38 +0300

    create variables with the const name

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 function birthDate(){
-  var year= document.getElementById("year").value;
-  var cc = parseInt(year.slice(0,2));
-  var yy= parseInt (year.slice(2.));
-  var dd= document.getElementById("date").value;
-  var mm= document.getElementById("month").value;
-
-}
+  const YEAR= document.getElementById("year").value;
+  const CC = parseInt(YEAR.slice(0,2));
+  const YY= parseInt (YEAR.slice(2.));
+  const DD= document.getElementById("date").value;
+  const MM= document.getElementById("month").value;
+  const myGender = document.getElementById("gender").value;
 
 var dayOfTheWeek= (((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd )% 7;
 var maleNames= ["Kwasi", "Kwadwo","Kwabena", "Kwaku", "Yaw","Kofi", "Kwame"];
 var femaleNames= ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ];
 var birthDays= ["sunday","monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-var myGender =document.getElementByName("gender").value;
+
 
 
 if(myGender==="Male")
