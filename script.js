df986022a5127717c4db60334e2961e7445c38c6
Brenda-M <brendanjuguna1@gmail.com>
2020-02-16 22:51:28 +0300

    changes background color

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-function birthDate(){
-  //obtain values entered in the forms
-
-  const DD= document.getElementById("date").value;
-  const MM= document.getElementById("month").value;
-  const YY= document.getElementById("year").value;
+function birthDate() {
+  // Get the values entered in the inputs
+  const DD = document.getElementById("date").value;
+  const MM = document.getElementById("month").value;
+  const YY = document.getElementById("year").value;
   const myGender = document.getElementById("gender").value;
 
   // Check if the values entered are numbers
@@ -38,24 +37,25 @@ function birthDate(){
   // Get the century digits
   const CC = YY.toString().substring(0, 2);
 
-  //Get the day of the week
-  const dayOfTheWeek = userDate.getDay();
+  // Create a date object from the users input
+  const userDate = new Date(YY, MM, DD);
 
-  const maleNames = ["Kwasi", "Kwadwo","Kwabena", "Kwaku", "Yaw","Kofi", "Kwame"];
-  const femaleNames= ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ];
-  const birthDays = ["sunday","monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+  // Get the day of the week
+  const dayOfTheWeek = userDate.getUTCDay();
 
+  const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  const birthDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
   const day = birthDays[dayOfTheWeek]
 
-
   let name = '';
 
-  if(myGender === "male"){
+  if (myGender === "male") {
     name = maleNames[dayOfTheWeek];
   } else {
     name = femaleNames[dayOfTheWeek];
   }
 
-  document.getElementById("display").innerHTML= `Your akan names is ${name} because you were born on a ${day}`;
+  document.getElementById("display").innerHTML = `Your akan name is ${name} because you were born on a ${day}`;
 }
